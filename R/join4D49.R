1e88f5c332734aab9e6cba3e84211f596114f3d7
jpkabala96 <101903735+jpkabala96@users.noreply.github.com>
2022-11-24 19:08:49 +0100

    Update join4D49.R

--- R/join4D49.R
+++ R/join4D49.R
@@ -9,15 +9,15 @@
 
 join4D49 <- function(data4D, data49){
   jdata <- dplyr::left_join(data4D %>%
-                              dplyr::select(-c("Record")),
+                              dplyr::select(-tidyselect::starts_with(c("Record"))),
                             data49 %>%
-                              dplyr::select(-c("date",
+                              dplyr::select(-tidyselect::starts_with(c("date",
                                                "f_hour",
                                                "hour",
                                                "timestamp",
                                                "Tipo_stringa",
                                                "Record",
-                                               "Record_number")),
+                                               "Record_number"))),
                             by = c("id" = "id",
                                    "date_hour" = "date_hour"))
 
